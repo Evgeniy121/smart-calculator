ab24ee0fdee2c623d1406311c51021a6199d198d
Evgeniy121 <36643137+Evgeniy121@users.noreply.github.com>
2018-03-05 18:28:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,94 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = [initialValue];
+    this.op = ['pow','*','/','-','+'];
   }
 
   add(number) {
-    // your implementation
+    this.value.push('+');
+    this.value.push(number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.value.push('-');
+    this.value.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.value.push('*');
+    this.value.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.value.push('/');
+    this.value.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.value.push('pow');
+    this.value.push(number);
+    return this;
+  }
+
+  toString(){
+    for(var i=0; i<this.op.length; i++)
+    {
+      var zn = this.op[i];
+      for(var j=0; j<this.value.length; j++)
+      {
+
+        if(this.value[j]==zn)
+        {
+        if(this.value[j]=='pow')
+         {
+          this.value[j] = Math.pow(this.value[j-1],this.value[j+1]);
+          this.value.splice(j+1, 1);
+          this.value.splice(j-1, 1);
+          j = 0;
+        }
+        if(this.value[j]=='*')
+        {
+          this.value[j] = this.value[j+1]*this.value[j-1];
+          this.value.splice(j+1, 1);
+          this.value.splice(j- 1, 1);
+          j = 0;
+        }
+        if(this.value[j]=='/')
+        {
+          this.value[j] = this.value[j-1]/this.value[j+1];
+          this.value.splice(j+ 1, 1);
+          this.value.splice(j-1, 1);
+          j = 0;
+        }
+
+        if(this.value[j]=='-')
+        {
+          this.value[j] = this.value[j-1]-this.value[j+1];
+          this.value.splice(j+1, 1);
+          this.value.splice(j-1, 1);
+          j = 0;
+        }
+        if(this.value[j]=='+')
+        {
+          this.value[j] = this.value[j+1]+this.value[j-1];
+          this.value.splice(j+1, 1);
+          this.value.splice(j-1, 1);
+          j = 0;
+        }
+        else{
+          continue;
+        }
+
+        j=0;
+        }
+      }
+    }
+
+    return this.value[0];
   }
 }
 
